f27f929d7f68af4164a92241145b7f096bf96308
warmkatahira <t.katahira@warm.co.jp>
2026-03-11 14:58:31 +0900

    20260311_1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <p class="text-xl mb-2"><i class="las la-search mr-1 la-lg"></i>ロケーション検索</p>
     <div class="flex flex-col">
-        <input type="tel" id="item_jan_code" name="item_jan_code" class="text-sm border border-black md:w-1/2 w-4/5" placeholder="JANコード入力(含むで検索されます)" autocomplete="off">
+        <input type="tel" id="item_jan_code" name="item_jan_code" class="text-base border border-black md:w-2/5 w-4/5" placeholder="JANコード入力(含むで検索されます)" autocomplete="off">
     </div>
     <div id="result" class="mt-3"></div>
 </x-app-layout>
